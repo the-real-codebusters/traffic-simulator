72f78711dabc018188c5f5725893ac0bedb23456
riedler <riedler@cip.ifi.lmu.de>
2021-01-07 11:05:03 +0100

    Canvas in ein ScrollPane gesetzt. Iso-Koordinaten passen immer noch nicht

--- src/view/View.java
+++ src/view/View.java
@@ -10,8 +10,11 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
 
 public class View {
     private Controller controller;
@@ -21,21 +24,35 @@ public class View {
     private final int tileWidthHalf = tileWidth / 2;
     private int tileHeight = 32;
     private final int tileHeightHalf = tileHeight / 2;
+    private final int MAP_SIZE = 9;
 
+    ScrollPane scrollPane = new ScrollPane();
+    Canvas canvas = new Canvas(1000, 800);
 
     public View(Stage primaryStage) {
         this.stage = primaryStage;
-        Canvas canvas = new Canvas(800, 600);
 
-        final int MAP_SIZE = 10;
+        scrollPane.setContent(canvas);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setFitToHeight(true);
+        scrollPane.setFitToWidth(true);
+        scrollPane.setPannable(true);
+
         Image image = new Image(getClass().getResource("/greentile.png").toString());
 
-        Label tileLabel = new Label();
-        tileLabel.setFont(new Font("Arial", 20));
+        Label isoCoordLabel = new Label();
+        isoCoordLabel.setFont(new Font("Arial", 15));
+
+        Label mousePosLabel = new Label();
+        mousePosLabel.setFont(new Font("Arial", 15));
 
         BorderPane root = new BorderPane();
-        root.setBottom(tileLabel);
-        root.setCenter(canvas);
+        root.setPrefSize(700, 500);
+        VBox vBox = new VBox();
+        root.setBottom(vBox);
+        vBox.getChildren().addAll(mousePosLabel, isoCoordLabel);
+        root.setCenter(scrollPane);
         GraphicsContext gc = canvas.getGraphicsContext2D();
 
         double canvasCenterWidth = canvas.getWidth() / 2;
@@ -53,15 +70,29 @@ public class View {
                 gc.drawImage(image, tileX + canvasCenterWidth - tileWidthHalf * MAP_SIZE, tileY + canvasCenterHeight);
 
                 canvas.setOnMouseClicked(event -> {
-                    double x = Math.floor(((event.getY()  / tileHeight) + ((event.getX()  / tileWidth))));
-                    double y = Math.floor(((event.getY()) / tileHeight) - ((event.getX()) / tileWidth));
+
+                    double x = findTileCoordX(event.getX(), event.getY());
+                    double y = findTileCoordY(event.getX(), event.getY());
+
+                    String mousePos = "Mouse coordinates: x: " + event.getX() + " y: " + event.getY();
+                    mousePosLabel.setText((mousePos));
 
                     String text = "Tile coordinates: x: " + x + " y: " + y;
-                    tileLabel.setText(text);
+                    isoCoordLabel.setText(text);
                 });
             }
         }
         this.stage.setScene(new Scene(root));
+    }
+
+    public double findTileCoordX(double mouseX, double mouseY){
+        double x = Math.floor(mouseY / tileHeight + mouseX  / tileWidth) - MAP_SIZE -1;
+        return x;
+    }
+
+    public double findTileCoordY(double mouseX, double mouseY){
+        double y = Math.floor(mouseX / tileWidth - mouseY / tileHeight) + MAP_SIZE -1;
+        return y;
     }
 
     public void setController(Controller controller) {
